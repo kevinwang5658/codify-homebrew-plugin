c034aa624eb73f01fcda812c5f9e8a017752e235
kevinwang <kevinwang5658@gmail.com>
2024-04-13 11:24:25 -0400

    New constants file for GUI applications

---
+++ src/resources/applications/constants.ts
@@ -0,0 +1,8 @@
+export const applicationUrls = {
+  INTELLIJ_WEBSTORM: 'https://download.jetbrains.com/webstorm/WebStorm-2024.1-aarch64.dmg',
+  INTELLIJ_IDEA: 'https://download.jetbrains.com/idea/ideaIU-2024.1-aarch64.dmg',
+  INTELLIJ_IDEA_COMMUNITY: 'https://download.jetbrains.com/idea/ideaIC-2024.1-aarch64.dmg',
+  INTELLIJ_PYCHARM: 'https://download.jetbrains.com/python/pycharm-professional-2024.1-aarch64.dmg',
+
+  SUBLIME: 'https://www.sublimetext.com/download_thanks?target=mac',
+}
