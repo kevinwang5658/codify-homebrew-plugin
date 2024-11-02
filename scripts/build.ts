e3d0e05994157bdd30dae06f8c323c655de644c2
kevinwang <kevinwang5658@gmail.com>
2024-11-02 18:49:12 -0400

    feat: add build scripts that performs rollup + generates JSON schema file

---
+++ scripts/build.ts
@@ -0,0 +1,97 @@
+import { JSONSchema } from '@apidevtools/json-schema-ref-parser';
+import { Ajv } from 'ajv';
+import { IpcMessage, IpcMessageSchema, MessageStatus, ResourceSchema } from 'codify-schemas';
+import mergeJsonSchemas from 'merge-json-schemas';
+import { ChildProcess, fork } from 'node:child_process';
+
+import { codifySpawn } from '../src/utils/codify-spawn.js';
+
+const ajv = new Ajv({
+  strict: true
+});
+const ipcMessageValidator = ajv.compile(IpcMessageSchema);
+
+function sendMessageAndAwaitResponse(process: ChildProcess, message: IpcMessage): Promise<any> {
+  return new Promise((resolve, reject) => {
+    process.on('message', (response: IpcMessage) => {
+      if (!ipcMessageValidator(response)) {
+        throw new Error(`Invalid message from plugin. ${JSON.stringify(message, null, 2)}`);
+      }
+
+      // Wait for the message response. Other messages such as sudoRequest may be sent before the response returns
+      if (response.cmd === message.cmd + '_Response') {
+        if (response.status === MessageStatus.SUCCESS) {
+          resolve(response.data)
+        } else {
+          reject(new Error(String(response.data)))
+        }
+      }
+    });
+
+    // Send message last to ensure listeners are all registered
+    process.send(message);
+  });
+}
+
+await codifySpawn('rm -rf ./dist')
+await codifySpawn('npm run rollup -- -f es');
+
+const plugin = fork(
+  './dist/index.js',
+  [],
+  {
+    // Use default true to test plugins in secure mode (un-able to request sudo directly)
+    detached: true,
+    env: { ...process.env },
+    execArgv: ['--import', 'tsx/esm', '--inspect=9221'],
+  },
+)
+
+const initializeResult = await sendMessageAndAwaitResponse(plugin, {
+  cmd: 'initialize',
+  data: {}
+})
+
+const { resourceDefinitions } = initializeResult;
+const resourceTypes = resourceDefinitions.map((i) => i.type);
+
+const schemasMap = new Map<string, JSONSchema>()
+for (const type of resourceTypes) {
+  const resourceInfo = await sendMessageAndAwaitResponse(plugin, {
+    cmd: 'getResourceInfo',
+    data: { type }
+  })
+
+  schemasMap.set(type, resourceInfo.schema);
+}
+
+const mergedSchemas = [...schemasMap.entries()].map(([type, schema]) => {
+    // const resolvedSchema = await $RefParser.dereference(schema)
+    const resourceSchema = JSON.parse(JSON.stringify(ResourceSchema));
+
+    delete resourceSchema.$id;
+    delete resourceSchema.$schema;
+    resourceSchema.description = `Resource type: "${type}" | ${resourceSchema.title}`;
+    delete resourceSchema.title;
+    delete resourceSchema.properties.type;
+
+    if (schema) {
+      delete schema.$id;
+      delete schema.$schema;
+      delete schema.title;
+    }
+
+    return mergeJsonSchemas([schema ?? {}, resourceSchema, { properties: { type: { const: type, type: 'string' } } }]);
+  });
+
+
+await codifySpawn('rm -rf ./dist')
+await codifySpawn('npm run rollup'); // re-run rollup without building for es
+
+console.log('JSON Schemas for all resources')
+console.log(JSON.stringify(mergedSchemas, null, 2));
+
+process.exit(0)
+
+
+
